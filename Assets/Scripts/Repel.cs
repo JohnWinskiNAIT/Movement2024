ebc8fab4c6bb3e3b3282730957a2824af62d4854
John Winski <JWINSKI@nait.ca>
2024-10-03 10:55:17 -0600

    Added explosion force

--- Assets/Scripts/Repel.cs
+++ Assets/Scripts/Repel.cs
@@ -20,13 +20,25 @@ public class Repel : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("collision");
+        float radius = 10.0F;
+        float power =  300.0F;
+        Vector3 explosionPos = transform.position;
         rbody = collision.gameObject.GetComponentInParent<Rigidbody>();
 
         if (rbody != null)
         {
             //rbody.AddRelativeForce(collision.gameObject.transform.position - transform.position * 1.0f, ForceMode.Impulse);
-            rbody.AddRelativeForce(Vector3.up * 100.0f, ForceMode.Impulse);
+            //rbody.AddForce(Vector3.up * 30.0f, ForceMode.Impulse);
+            //rbody.AddExplosionForce(300.0f, transform.position, 100.0f, 3.0f);
+
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponentInParent<Rigidbody>();
+
+                if (rb != null)
+                    rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+            }
         }
     }
 }
